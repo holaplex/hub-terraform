a32e46e9235a9b5ec90e779fc719a3bfb6467135
mpw <x@mpw.sh>
2023-04-05 21:59:00 -0300

    expose cidr in values.yaml

--- router.tf
+++ router.tf
@@ -1,6 +1,6 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router
 resource "google_compute_router" "router" {
-  name    = "router"
-  region  = "us-central1"
+  name    = local.values.router.name
+  region  = local.values.vpc.region
   network = google_compute_network.main.id
 }
--- subnets.tf
+++ subnets.tf
@@ -1,17 +1,17 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
 resource "google_compute_subnetwork" "private" {
-  name                     = "private"
-  ip_cidr_range            = "10.0.0.0/18"
-  region                   = "us-central1"
+  name                     = local.values.subnetwork.name
+  ip_cidr_range            = local.values.subnetwork.cidrRange
+  region                   = local.values.vpc.region
   network                  = google_compute_network.main.id
-  private_ip_google_access = true
+  private_ip_google_access = local.values.subnetwork.privateIpGoogleAccess
 
   secondary_ip_range {
     range_name    = "k8s-pod-range"
-    ip_cidr_range = "10.48.0.0/14"
+    ip_cidr_range = local.values.kubernetes.networkRanges.pods
   }
   secondary_ip_range {
     range_name    = "k8s-service-range"
-    ip_cidr_range = "10.52.0.0/20"
+    ip_cidr_range = local.values.kubernetes.networkRanges.services
   }
 }
