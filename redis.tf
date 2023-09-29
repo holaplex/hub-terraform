6f63dcc7936e726e83ec54e94c4676d30b30d194
mpw <x@mpw.sh>
2023-09-28 21:05:51 -0300

    add redis deployment for hub-nfts background worker

---
+++ redis.tf
@@ -0,0 +1,37 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/redis_instance
+resource "google_redis_instance" "redis" {
+  for_each = {
+    for index, instance in local.values.redisInstances :
+    instance.name => instance
+  }
+
+  name           = each.value.name
+  display_name   = each.value.name
+  region         = local.values.vpc.region
+  tier           = each.value.tier
+  memory_size_gb = each.value.memorySizeGb
+
+  authorized_network = google_compute_network.main.id
+
+  redis_version = "REDIS_7_0"
+
+  maintenance_policy {
+    weekly_maintenance_window {
+      day = "TUESDAY"
+      start_time {
+        hours   = 0
+        minutes = 30
+        seconds = 0
+        nanos   = 0
+      }
+    }
+  }
+
+}
+
+output "redis_instance_ips" {
+  value = {
+    for instance in google_redis_instance.redis :
+    instance.name => instance.host
+  }
+}
